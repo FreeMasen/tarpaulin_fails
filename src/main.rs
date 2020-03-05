9916847115683667b5549ddf0ed7b0305267c5c9
Robert Masen <robertmasen@mn-rmasen-m.ad.smartthings.com>
2020-03-05 12:01:15 -0600

    Attempt to reproduce the cargo panic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use cargo::util::{Config, homedir};
+use cargo::core::{Workspace, Shell};
+use std::path::{Path, PathBuf};
+
 fn main() {
-    println!("Hello, world!");
+    let cwd = PathBuf::from("");
+    let hd = homedir(Path::new(&cwd)).unwrap();
+    let mut cfg = Config::new(Shell::new(), cwd, hd);
+    cfg.configure(
+        0u32,
+        false,
+        None,
+        false,
+        false,
+        false,
+        &None,
+        &[],
+        &[]
+    ).unwrap();
+    let _workspace = Workspace::new(&Path::new("Cargo.toml"), &cfg);
 }
